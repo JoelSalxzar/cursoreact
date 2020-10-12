6f0dafe866699668f86c93ee445016377f118f9b
Joel Salazar <joelsalazar.isc@hotmail.com>
2020-10-12 14:23:01 -0500

    usando ant design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { DatePicker } from 'antd';
 import './App.scss';
 
 function App() {
+  const test = (date, dateString) => {
+    console.log(date, dateString)
+  }
   return (
     <div className="App">
-    <h1>Web personal</h1>
+    <h1>Web personal de Joel</h1>
+  
+    <DatePicker onChange={test}/>
+  
     </div>
   );
 }
