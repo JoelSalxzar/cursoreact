3d87f4da2167e17db24666db4cd59eacb348414c
Joel Salazar <joelsalazar.isc@hotmail.com>
2020-11-11 12:29:21 -0600

    Yacasilaptaseccion7meodio

--- src/components/Admin/RegisterForm/RegisterForm.js
+++ src/components/Admin/RegisterForm/RegisterForm.js
@@ -20,96 +20,109 @@ export default function RegisterForm(){
     });
 
     const changeForm = e => {
-        if(e.target.name === "privacyPolicy")
-        setInputs({
-                ...inputs,
-                [e.target.name]: e.target.checked
-            });
-        else{
-            setInputs({
-                ...inputs,
-                [e.target.name]: e.target.value
-            });
+        if (e.target.name === "privacyPolicy") {
+          setInputs({
+            ...inputs,
+            [e.target.name]: e.target.checked
+          });
+        } else {
+          setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+          });
         }
-    };
+      };
 
-    const inputValidation= e =>{
-        const {type, name} = e.target;
-
-        if(type === "email"){
-            setFormValid({
-                ...formValid,
-                [name]: emailValidation(e.target)
-            });
-        } 
-
-        if(type === "password"){
-            setFormValid({
-                ...formValid,
-                [name]: minLengthValidation(e.target, 6)
-            });
+    const inputValidation = e => {
+        const { type, name } = e.target;
+    
+        if (type === "email") {
+          setFormValid({ ...formValid, [name]: emailValidation(e.target) });
         }
-
-        if( type === "checkbox"){
-            setFormValid({
-                ...formValid,
-                [name]: e.target.checked
-            })
+        if (type === "password") {
+          setFormValid({ ...formValid, [name]: minLengthValidation(e.target, 1) });
         }
-    }
+        if (type === "checkbox") {
+          setFormValid({ ...formValid, [name]: e.target.checked });
+        }
+      };
+    
 
     const register = e =>{
-        e.preventDefault();
+         e.preventDefault();
+         const {email, password, repeatPassword, privacyPolicy} = formValid;
 
-    }
-    return(
-        <Form className="register-form" onSubmit={register} onChange={changeForm}>
-            <Form.Item>
-                <Input 
-                    prefix={<Icon type="user" style={{color: "rgba(0,0,0,.25)"}} />}
-                    type="email"
-                    name="email"
-                    placeholder="Corre electrónico"
-                    className="register-form__input"
-                    value={inputs.email}
-                />
-            </Form.Item>
-            <Form.Item>
-                <Input
-                    prefix={<Icon type="lock" style={{color: "rgba(0,0,0,.25)"}} />}
-                    type="password"
-                    name="password"
-                    placeholder="Contraseña"
-                    className="register-form__input"
-                    onChange={inputValidation}
-                    value={inputs.password}
-                />
-            </Form.Item>
-            <Form.Item>
-                <Input 
-                    prefix={<Icon type="lock" style={{color: "rgba(0,0,0,.25)"}} />}
-                    type="password"
-                    name="repeatPassword"
-                    placeholder="Repetir Contraseña"
-                    className="register-form__input"
-                    onChange={inputValidation}
-                    value={inputs.repeatPassword}
-                />
-            </Form.Item>
-            <Form.Item>
-                <Checkbox
-                    name="privacyPolicy"
-                    checked={inputs.privacyPolicy}
-                    onChange={inputValidation} 
-                >
-                    He leído y acepto la política de privacidad.
-                </Checkbox>
-            </Form.Item>
-            <Form.Item>
-                <Button htmlType="submit" className="register-form__button">
-                    Crear cuenta
-                </Button>
-            </Form.Item>
-        </Form>
-    )
+         const emailVal = inputs.email;
+         const passwordVal = inputs.password;
+         const repeatPasswordVal = inputs.repeatPassword;
+         const privacyPolicyVal = inputs.privacyPolicy;
+
+         if(!emailVal|| !passwordVal || !repeatPasswordVal || !privacyPolicyVal) {
+            notification['error']({
+                message: "Todos los campos son obligatorios"
+            });
+         }else{
+           if(passwordVal !== repeatPasswordVal){
+              notification["error"]({
+                message: "Las contraseñas tienen que ser iguales."
+              });
+           }else{
+            notification["success"]({
+              message: "Las contraseñas tienen que ser iguales"
+            });
+           }
+         }
+    };
+
+  return (
+    <Form className="register-form" onSubmit={register} onChange={changeForm}>
+      <Form.Item>
+        <Input
+          prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+          type="email"
+          name="email"
+          placeholder="Correo electronico"
+          className="register-form__input"
+          onChange={inputValidation}
+          value={inputs.email}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Input
+          prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          className="register-form__input"
+          onChange={inputValidation}
+          value={inputs.password}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Input
+          prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+          type="password"
+          name="repeatPassword"
+          placeholder="Repetir contraseña"
+          className="register-form__input"
+          onChange={inputValidation}
+          value={inputs.repeatPassword}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Checkbox
+          name="privacyPolicy"
+          onChange={inputValidation}
+          checked={inputs.privacyPolicy}
+        >
+          He leído y acepto la política de privacidad.
+        </Checkbox>
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType="submit" className="register-form__button">
+          Crear cuenta
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
